1c8ea4428a5200562b68a07957b05adab8d2df49
inerune <inerune72@gmail.com>
2025-07-03 01:43:42 +0500

    fix issue

--- app/components/workbench/terminal/TerminalTabs.tsx
+++ app/components/workbench/terminal/TerminalTabs.tsx
@@ -86,7 +86,7 @@ export const TerminalTabs = memo(() => {
     >
       {
         !showTerminal && <div onClick={() => workbenchStore.toggleTerminal(true)} className='py-2 px-4 dark:bg-[#1D2125] w-full border-y border-[#34383B] absolute bottom-0 z-9 text-white cursor-pointer flex items-center gap-1 '>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffff" viewBox="0 0 256 256"><path d="M117.31,134l-72,64a8,8,0,1,1-10.63-12L100,128,34.69,70A8,8,0,1,1,45.32,58l72,64a8,8,0,0,1,0,12ZM216,184H120a8,8,0,0,0,0,16h96a8,8,0,0,0,0-16Z"></path></svg>
+          <i className="ph ph-terminal"></i>
           <p className='text-sm'>Console</p>
         </div>
       }
