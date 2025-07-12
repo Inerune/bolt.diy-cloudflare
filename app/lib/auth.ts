b90495379d87a6efbca017deb0f2c5fd291f4636
inerune <inerune72@gmail.com>
2025-07-11 17:22:43 -0700

    fix the mongodb-errors

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,20 +1,29 @@
-import { betterAuth } from "better-auth"
-import { MongoClient } from "mongodb";
+import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URL as string);
-const db = client.db();
+let client: MongoClient;
+let db: Awaited<ReturnType<MongoClient["db"]>>;
 
+async function connectToMongo() {
+  if (!client) {
+    client = new MongoClient(process.env.MONGO_URL as string);
+    await client.connect();
+    db = client.db(); // optional: pass your DB name as arg
+  }
+  return db;
+}
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db),
-    emailAndPassword: {  
-        enabled: true
+  sessionSecret: process.env.SESSION_SECRET!,
+  database: mongodbAdapter(await connectToMongo()),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
-    socialProviders: { 
-        google: { 
-           clientId: process.env.GOOGLE_CLIENT_ID as string, 
-           clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
-    }, 
+  },
 });
