09002b6d8c21094e15c9f5e121c842cea13446e8
inerune <inerune72@gmail.com>
2025-07-16 12:28:18 -0700

    change the body font

--- uno.config.ts
+++ uno.config.ts
@@ -114,7 +114,7 @@ export default defineConfig({
   ],
   theme: {
     fontFamily: {
-      sans: ['sf-light', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      sans: ['SF-Pro-Text', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       heading: ['Fraine', 'Inter', 'ui-sans-serif', 'system-ui'],
       custom: ['sf-regular', 'Inter']
     },
